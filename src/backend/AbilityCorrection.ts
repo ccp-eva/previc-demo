9a52c1b2b6d4bc6cc8814d2ca5705b460d83a8e8
Jonas Engicht <dev@condecon.de>
2023-10-17 11:11:30 +0200

    fix estimation uniform response pattern

--- src/backend/AbilityCorrection.ts
+++ src/backend/AbilityCorrection.ts
@@ -1,33 +1,31 @@
 import { MaximumLikelihoodEstimator } from "adaptivetesting";
 
-export class AbilityCorrection{
-    public ml_estimator:MaximumLikelihoodEstimator;
+export class AbilityCorrection {
+    public ml_estimator: MaximumLikelihoodEstimator;
 
-    public constructor(){
+    public constructor() {
         this.ml_estimator = new MaximumLikelihoodEstimator();
-    }    
+    }
     public GetMaximumLikelihoodEstimation(ResponsePattern: number[], ItemDifficulties: number[]): number {
-        let estimation_result:number;
+        let estimation_result: number;
 
-        try{
+        try {
             estimation_result = this.ml_estimator.GetMaximumLikelihoodEstimation(ResponsePattern, ItemDifficulties);
         }
-        catch(e){
-            //override ability estimation when only one item has been answered yet
-            if(ResponsePattern.length == 1){
-            //Response 0
-            if(ResponsePattern[0] == 0){
-                estimation_result = -10;
+        catch (e) {
+            //override ability estimation when only one type of response has been given
+            //test if all responses are 0
+            if (ResponsePattern.every(p => p == 0)) {
+                return -10;
             }
-            //Response 1
-            else{
-                estimation_result = +10;
+            //all responses are 1
+            if (ResponsePattern.every(p => p == 1)) {
+                return +10
             }
-        }
-        }
-        
 
-        
+            //else: throw error
+            throw new Error("Ability estimation error.");
+        }
 
         return estimation_result!;
     }
